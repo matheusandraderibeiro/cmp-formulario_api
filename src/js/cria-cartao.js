438735a67839a0293afb78aadba87b151d257342
matheusandraderibeiro <matheus.a.r.01020@gmail.com>
2023-05-01 14:10:45 -0300

    adição de img

--- src/js/cria-cartao.js
+++
@@ -1,7 +0,0 @@
-const btnEnviar = document.getElementById ("enviar");
-
-btnEnviar.addEventListener ("click", () => {
-    const cartao = document.querySelector (".cartao");
-
-    console.log (cartao)
-});
